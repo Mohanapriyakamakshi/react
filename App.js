808f99171b6ff2c1c4447a86e551783f9300d39e
mohanapriya123 <161128399+Mohanapriyakamakshi@users.noreply.github.com>
2024-02-26 20:17:03 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+// File: App.js
+
+import React from 'react';
+import MyTextComponent from './MyTextComponent';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <MyTextComponent />
+    </div>
+  );
+};
+
+export default App;
